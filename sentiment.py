e84d2db2dfe023f01ff1b5c351d942c9b88c12da
Игорь Загальский <izagalskiy@mail.ru>
2022-12-02 19:32:11 +0300

    Add fastapi libraries

--- sentiment.py
+++ sentiment.py
@@ -1,11 +1,16 @@
+from fastapi import FastAPI
 from transformers import pipeline
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    text: str
 
 classifier = pipeline("sentiment-analysis", "blanchefort/rubert-base-cased-sentiment")
 
-classifier("Я обожаю инженерию машинного обучения!")
-classifier("Я обожаю собачек!")
-classifier("Я пытаюсь изучить GitHub!!!")
-classifier("Я люблю Git") #Проба пера
-classifier("Я изучаю инженерию машинного обучения") 
+# classifier("Я обожаю инженерию машинного обучения!")
+# classifier("Я обожаю собачек!")
+# classifier("Я пытаюсь изучить GitHub!!!")
+# classifier("Я люблю Git") #Проба пера
+# classifier("Я изучаю инженерию машинного обучения") 
 # --- для отката изменений необходимо ввести команду в терминале: git revert <Номер временной шкалы>
 # для изменения имени набрать в терминале git config --global user.name "FIRST_NAME LAST_NAME"
